cded2f8a9e3a9dc0ff914e7a2253ad14a3463382
Leofelixll <fleo16leo@gmail.com>
2023-10-18 11:43:12 -0300

    first commit

---
+++ atividade09.py
@@ -0,0 +1,9 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+
+numero1 = int(input('digite o primeiro numero:'))
+numero2 = int(input('digite o segundo numero:'))
+
+if numero1 > numero2:
+    print( numero1 )
+else:
+    print( numero2 )
